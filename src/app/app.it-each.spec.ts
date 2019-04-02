a2a889079803ef8c112feae8055d1d036e9dc8ad
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-04-02 14:11:54 +0200

    test: show tagged template syntax for it.each

--- src/app/app.it-each.spec.ts
+++ src/app/app.it-each.spec.ts
@@ -7,4 +7,18 @@ describe('it.each', () => {
       }
     );
   });
+
+  describe('When you want to have a better overview over your data', () => {
+    it.each`
+      name           | age
+      ${'Scott'}     | ${18}
+      ${'Peterson'}  | ${42}
+      ${'Leicester'} | ${93}
+      ${'Anderson'}  | ${38}
+    `('you should use the tagged template syntax ($name, $age)', customer => {
+      // Be aware that you TypeScript is not able to automatically infer
+      // the type of your test data now.
+      expect(customer.name.length).toBeGreaterThan(3);
+    });
+  });
 });
