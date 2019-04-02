be608b2863d42ff9f56b3eb38d93b8e797f81f02
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-04-02 13:25:18 +0200

    test: combine toContainEqual with objectContaining

--- src/app/app.expect-object.spec.ts
+++ src/app/app.expect-object.spec.ts
@@ -5,5 +5,18 @@ describe('expect.objectContaining', () => {
 
       expect(customer).toEqual(expect.objectContaining({ name: 'Anderson' }));
     });
+
+    it('you may want to combine it with .toContainEqual', () => {
+      const collection = [
+        { name: 'Scott', age: 18 },
+        { name: 'Peterson', age: 42 },
+        { name: 'Leicester', age: 93 },
+        { name: 'Anderson', age: 38 }
+      ];
+
+      expect(collection).toContainEqual(
+        expect.objectContaining({ name: 'Anderson' })
+      );
+    });
   });
 });
