b39da253092ab1fe708287126b356d48b481ad5b
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-04-02 12:34:25 +0200

    docs(component:app): explain why snapshot works

--- src/app/app.snapshot.spec.ts
+++ src/app/app.snapshot.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('<app-root>', () => {
@@ -7,15 +8,22 @@ describe('<app-root>', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent]
-    });
+    }).compileComponents();
 
     sut = TestBed.createComponent(AppComponent);
     sut.detectChanges();
   });
 
-  describe('When the application stats', () => {
-    it('displays the welcome message', () => {
+  describe('When the application starts', () => {
+    it('renders the whole component', () => {
+      // ComponentFixture is serialized by jest-preset-angular
       expect(sut).toMatchSnapshot();
+    });
+
+    it('displays the welcome message', () => {
+      // HTML is also serialized by jest-preset-angular
+      const welcomeMessage = sut.debugElement.query(By.css('p')).nativeElement;
+      expect(welcomeMessage).toMatchSnapshot();
     });
   });
 });
